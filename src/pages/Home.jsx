394fa57e27e4ae272ccd228056ee7ffcb19296bd
bestonemitRam <ramprakash.flutterdeveloper@gmail.com>
2025-11-27 12:27:59 +0530

    add contact us form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,7 @@ import SustainabilitySection from "../components/homepage/SustainabilitySection"
 import MediaCSRSection from "../components/homepage/MediaCSRSection";
 import CompanyStrip from "../components/common/CompanyStrip";
 import Footer from "../layouts/Footer";
+import ContactUs from "../components/homepage/ContactUs";
 
 export default function HomePage() {
     return (
@@ -47,6 +48,7 @@ export default function HomePage() {
             <SustainabilitySection />
             <MediaCSRSection />
             <CompanyStrip />
+            <ContactUs />
             <Footer />
 
 
